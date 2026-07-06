6d6d07267e3b864f15d6dc55e62aa22f80f523fc
indrolend.com <indrolend@gmail.com>
2026-07-06 18:58:52 -0500

    Add native core build script

---
+++ native/build_core.ps1
@@ -0,0 +1,20 @@
+$ErrorActionPreference = "Stop"
+
+$Root = Split-Path -Parent $PSScriptRoot
+$BuildDir = Join-Path $PSScriptRoot "build"
+
+New-Item -ItemType Directory -Force $BuildDir | Out-Null
+
+$cxx = if (Get-Command clang++ -ErrorAction SilentlyContinue) { "clang++" } else { "g++" }
+
+& $cxx `
+  -std=c++17 `
+  -Wall `
+  -Wextra `
+  -pedantic `
+  -I "$PSScriptRoot/core" `
+  "$PSScriptRoot/core/simulation.cpp" `
+  "$PSScriptRoot/tests/sim_smoke_test.cpp" `
+  -o "$BuildDir/db_sim_smoke.exe"
+
+& "$BuildDir/db_sim_smoke.exe"
