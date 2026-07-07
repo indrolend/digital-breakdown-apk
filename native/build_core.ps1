c1b2035dcada32b3b8b1f2c6354663136d998bc8
indrolend.com <indrolend@gmail.com>
2026-07-06 19:08:45 -0500

    Compile native render state in core smoke script

--- native/build_core.ps1
+++ native/build_core.ps1
@@ -1,8 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-$Root = Split-Path -Parent $PSScriptRoot
 $BuildDir = Join-Path $PSScriptRoot "build"
-
 New-Item -ItemType Directory -Force $BuildDir | Out-Null
 
 $cxx = if (Get-Command clang++ -ErrorAction SilentlyContinue) { "clang++" } else { "g++" }
@@ -14,6 +12,7 @@ $cxx = if (Get-Command clang++ -ErrorAction SilentlyContinue) { "clang++" } else
   -pedantic `
   -I "$PSScriptRoot/core" `
   "$PSScriptRoot/core/simulation.cpp" `
+  "$PSScriptRoot/core/render_state.cpp" `
   "$PSScriptRoot/tests/sim_smoke_test.cpp" `
   -o "$BuildDir/db_sim_smoke.exe"
 
