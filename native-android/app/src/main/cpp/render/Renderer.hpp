71f1b321efe90e466772d4244d82b0ed7c9ec5b2
indrolend.com <indrolend@gmail.com>
2026-07-08 10:45:30 -0500

    Add native OpenGL renderer interface

---
+++ native-android/app/src/main/cpp/render/Renderer.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "../game/Game.hpp"
+
+class Renderer {
+public:
+    void surfaceCreated();
+    void surfaceChanged(int width, int height);
+    void draw(const GameState& state);
+
+private:
+    int width_ = 1;
+    int height_ = 1;
+    unsigned int program_ = 0;
+    unsigned int vbo_ = 0;
+    int uMvp_ = -1;
+    int uColor_ = -1;
+    int aPos_ = -1;
+
+    bool initProgram();
+    void drawBox(const float* viewProj, const Vec3& pos, const Vec3& scale, float yaw, const float color[4]);
+    void drawGround(const float* viewProj);
+};
