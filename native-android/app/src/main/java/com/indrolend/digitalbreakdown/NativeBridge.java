09d75fb5ca964f9b856bc794ec5b4db76352d5fc
indrolend.com <indrolend@gmail.com>
2026-07-10 08:21:34 -0500

    Bridge native Android touch controls

--- native-android/app/src/main/java/com/indrolend/digitalbreakdown/NativeBridge.java
+++ native-android/app/src/main/java/com/indrolend/digitalbreakdown/NativeBridge.java
@@ -7,5 +7,17 @@ public final class NativeBridge {
     public static native void onSurfaceChanged(int width, int height);
     public static native void onDrawFrame();
     public static native void onTouch(int action, float x, float y, int pointerCount);
+    public static native void onTouchControls(
+        float moveX,
+        float moveZ,
+        float lookDeltaX,
+        float lookDeltaY,
+        boolean vacuumHeld,
+        boolean sprintHeld,
+        boolean jumpPressed,
+        boolean meleePressed,
+        boolean shootPressed,
+        boolean cameraTogglePressed
+    );
     public static native void onKey(int keyCode, boolean down);
 }
