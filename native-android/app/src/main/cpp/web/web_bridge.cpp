b7e47b9f9aaccd9c6a008a7a28ae5446f131d8b1
indrolend.com <indrolend@gmail.com>
2026-07-12 10:57:16 -0500

    Add native WebAssembly host bridge

---
+++ native-android/app/src/main/cpp/web/web_bridge.cpp
@@ -0,0 +1,86 @@
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+
+#include "../game/Game.hpp"
+#include "../render/Renderer.hpp"
+
+namespace {
+Game gGame;
+Renderer gRenderer;
+EMSCRIPTEN_WEBGL_CONTEXT_HANDLE gContext = 0;
+bool gReady = false;
+}
+
+extern "C" {
+
+EMSCRIPTEN_KEEPALIVE int db_init(const char* canvasSelector) {
+    if (gReady) return 1;
+
+    EmscriptenWebGLContextAttributes attributes;
+    emscripten_webgl_init_context_attributes(&attributes);
+    attributes.alpha = EM_FALSE;
+    attributes.depth = EM_TRUE;
+    attributes.antialias = EM_TRUE;
+    attributes.majorVersion = 1;
+    attributes.minorVersion = 0;
+
+    gContext = emscripten_webgl_create_context(canvasSelector, &attributes);
+    if (gContext <= 0) return 0;
+    if (emscripten_webgl_make_context_current(gContext) != EMSCRIPTEN_RESULT_SUCCESS) return 0;
+
+    gGame.reset();
+    gRenderer.surfaceCreated();
+    gReady = true;
+    return 1;
+}
+
+EMSCRIPTEN_KEEPALIVE void db_resize(int width, int height) {
+    if (!gReady) return;
+    gRenderer.surfaceChanged(width, height);
+}
+
+EMSCRIPTEN_KEEPALIVE void db_frame(float dt) {
+    if (!gReady) return;
+    gGame.update(dt);
+    gRenderer.draw(gGame.state());
+}
+
+EMSCRIPTEN_KEEPALIVE void db_key(int keyCode, int down) {
+    gGame.setKey(keyCode, down != 0);
+}
+
+EMSCRIPTEN_KEEPALIVE void db_touch_controls(
+    float moveX,
+    float moveZ,
+    float lookDeltaX,
+    float lookDeltaY,
+    int vacuumHeld,
+    int sprintHeld,
+    int jumpPressed,
+    int meleePressed,
+    int shootPressed,
+    int cameraTogglePressed
+) {
+    gGame.setTouchControls(
+        moveX,
+        moveZ,
+        lookDeltaX,
+        lookDeltaY,
+        vacuumHeld != 0,
+        sprintHeld != 0,
+        jumpPressed != 0,
+        meleePressed != 0,
+        shootPressed != 0,
+        cameraTogglePressed != 0
+    );
+}
+
+EMSCRIPTEN_KEEPALIVE const char* db_source_commit() {
+#ifdef DIGITAL_BREAKDOWN_SOURCE_COMMIT
+    return DIGITAL_BREAKDOWN_SOURCE_COMMIT;
+#else
+    return "unknown";
+#endif
+}
+
+}
