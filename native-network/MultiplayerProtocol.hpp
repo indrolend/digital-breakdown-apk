d5f1b7fa50ed3d5465cc585eacc134c04b76018b
indrolend.com <indrolend@gmail.com>
2026-07-20 14:49:32 -0500

    Use generated compatibility versions

--- native-network/MultiplayerProtocol.hpp
+++ native-network/MultiplayerProtocol.hpp
@@ -6,12 +6,13 @@
 #include <vector>
 
 #include "Game.hpp"
+#include "GeneratedBuildIdentity.hpp"
 
 namespace dbnet {
 
 constexpr std::uint32_t MAGIC = 0x504d4244u;
-constexpr std::uint16_t PROTOCOL_VERSION = 4;
-constexpr std::uint16_t GAMEPLAY_VERSION = 4;
+constexpr std::uint16_t PROTOCOL_VERSION = dbbuild::PROTOCOL_VERSION;
+constexpr std::uint16_t GAMEPLAY_VERSION = dbbuild::GAMEPLAY_VERSION;
 constexpr std::size_t HEADER_BYTES = 20;
 constexpr std::size_t MAX_PACKET_BYTES = 64u * 1024u;
 constexpr int MAX_PLAYERS = 4;
